7b78f7bbff6b4a85c503f07f5f6ae560d0f82000
Mckaty <mckaty.gm@gmail.com>
2023-07-04 01:00:58 +0700

    Update decrypt-gpg-s3-uat.sh
    archive

--- decrypt-gpg-s3-uat.sh
+++ decrypt-gpg-s3-uat.sh
@@ -27,7 +27,8 @@ done
 #rm /tmp/decrypted-files/*.pgp
 
 # Move all PGP files to the archive directory
-aws s3 mv "s3://${S3_BUCKET}/${S3_INPUT_PATH}/" "s3://${S3_BUCKET}/${S3_ARCHIVE_PATH}/" --recursive --exclude "*" --include "*.pgp"
+#aws s3 mv "s3://${S3_BUCKET}/${S3_INPUT_PATH}/" "s3://${S3_BUCKET}/${S3_ARCHIVE_PATH}/" --recursive --exclude "*" --include "*.pgp"
+aws s3 mv s3://poc-gpg-bucket-uat-142603072023/in/ s3://poc-gpg-bucket-uat-142603072023/archive/ --recursive --exclude "*" --include "*.pgp"
 
 # Display a success message
 echo "Decryption complete. Files saved in ${LOCAL_DIR}"
